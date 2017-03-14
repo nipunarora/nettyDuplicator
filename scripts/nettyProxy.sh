d6cf3e4729bfaae561a2ac80e66fa2a01c79baef
Nipun Arora <nipun@nec-labs.com>
2017-03-14 12:25:09 -0400

    updated nettyproxy for debug

--- scripts/nettyProxy.sh
+++ scripts/nettyProxy.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-java -cp target/nettyproxy-1.0.jar:target/netty-all-4.1.8.Final.jar org.columbia.parikshan.Main -l 0.0.0.0:1357 -o 127.0.0.1:1358 -r 127.0.0.1:1356
+java -cp target/nettyproxy-1.0.jar:target/netty-all-4.1.8.Final.jar org.columbia.parikshan.Main $@
+#-l 0.0.0.0:3379 -o 127.0.0.1:3380
+#-r 138.15.170.35:3380
